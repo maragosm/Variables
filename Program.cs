f666b7a8cb86ddce65d0e3ff1e2cd62ffa396923
Michael Maragos <maragosm@aol.com>
2022-06-01 10:14:36 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string dogName = "Ralph";
+int dogAge = 10;
+
+Console.WriteLine($"My dog's name is {dogName}. He is {dogAge} years old.");
